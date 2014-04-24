89f9b6248311b142b3b7a4de5f0866e3b7af111c
Christian A. Schlubach <christian.schlubach@gmail.com>
2014-04-23 21:36:49 -0400

    boundary condition
    just in case i have time tomorrow.

--- Blob.js
+++ Blob.js
@@ -56,6 +56,17 @@ Blob.prototype.redrawBlob = function() {
 };
 
 
+Blob.prototype.collision = function() {
+// function isCollide(a, b) {
+//     return !(
+//         ((a.y + a.height) < (b.y)) ||
+//         (a.y > (b.y + b.height)) ||
+//         ((a.x + a.width) < b.x) ||
+//         (a.x > (b.x + b.width))
+//     );
+// }
+}
+
 /*****************************************************************************
 * Handling our Radius
 */
